1664322581211b1d9b1beaffb8d0c811438cd096
su-mong <sumong.woo@ourtube.co.kr>
2023-12-15 23:26:33 +0900

    init

--- src/Main.java
+++ src/Main.java
@@ -53,25 +53,25 @@ public class Main {
         try (Connection connection = DriverManager.getConnection(Constants.dbUrl,Constants.dbUser, Constants.dbPassword)) {System.out.println("Connection established successfully");
 
             // PreparedStatement preparedStatement = connection.prepareStatement(SQL_INSERT)) {
-            createT = "CREATE TABLE IF NOT EXISTS zzz_users (id SERIAL PRIMARY KEY,name VARCHAR(100) NOT NULL,user_id VARCHAR(20) UNIQUE NOT NULL,password VARCHAR(100) NOT NULL,email VARCHAR(100) UNIQUE NOT NULL,notification_channel VARCHAR(100));";
+            createT = "CREATE TABLE IF NOT EXISTS users (id SERIAL PRIMARY KEY,name VARCHAR(100) NOT NULL,user_id VARCHAR(20) UNIQUE NOT NULL,password VARCHAR(100) NOT NULL,email VARCHAR(100) UNIQUE NOT NULL,notification_channel VARCHAR(100));";
             smt = connection.createStatement();
             smt.executeUpdate(createT);
 
-            createT = "CREATE TABLE IF NOT EXISTS zzz_event_infos (id SERIAL PRIMARY KEY,host_id SERIAL NOT NULL,CONSTRAINT fk_events_host_id FOREIGN KEY(host_id) REFERENCES users(id),event_title VARCHAR(255) NOT NULL,start_date_time TIMESTAMP WITH TIME ZONE NOT NULL,end_date_time TIMESTAMP WITH TIME ZONE NOT NULL,color VARCHAR(7) NOT NULL,description TEXT);";
-            smt = connection.createStatement();
-            smt.executeUpdate(createT);
-
-
-            createT = "CREATE TABLE IF NOT EXISTS zzz_events (id SERIAL PRIMARY KEY,event_info_id SERIAL NOT NULL,CONSTRAINT fk_event_info_id FOREIGN KEY(event_info_id) REFERENCES event_infos(id),user_id SERIAL NOT NULL,CONSTRAINT fk_events_user_id FOREIGN KEY(user_id) REFERENCES users(id),color VARCHAR(7) NOT NULL,reminder_interval INT DEFAULT 0,  reminder_time_frame INT );";
-            smt = connection.createStatement();
-            smt.executeUpdate(createT);
-
-            createT = "CREATE TABLE IF NOT EXISTS zzz_reminders (id SERIAL PRIMARY KEY,event_id SERIAL,CONSTRAINT fk_reminders_event_id FOREIGN KEY(event_id) REFERENCES events(id),remind_at TIMESTAMP WITH TIME ZONE NOT NULL,showed_reminder BOOLEAN DEFAULT FALSE);";
+            createT = "CREATE TABLE IF NOT EXISTS event_infos (id SERIAL PRIMARY KEY,host_id SERIAL NOT NULL,CONSTRAINT fk_events_host_id FOREIGN KEY(host_id) REFERENCES users(id),event_title VARCHAR(255) NOT NULL,start_date_time TIMESTAMP WITH TIME ZONE NOT NULL,end_date_time TIMESTAMP WITH TIME ZONE NOT NULL,color VARCHAR(7) NOT NULL,description TEXT);";
             smt = connection.createStatement();
             smt.executeUpdate(createT);
 
 
-            createT = "CREATE TABLE IF NOT EXISTS zzz_rsvps (id SERIAL PRIMARY KEY,event_id SERIAL,CONSTRAINT fk_rsvps_event_id FOREIGN KEY(event_id) REFERENCES events(id),rsvp_sender_id SERIAL,CONSTRAINT fk_rsvps_sender_id FOREIGN KEY(rsvp_sender_id) REFERENCES users(id),rsvp_receiver_id SERIAL,CONSTRAINT fk_rsvps_receiver_id FOREIGN KEY(rsvp_receiver_id) REFERENCES users(id),  received_at TIMESTAMP WITH TIME ZONE NOT NULL,is_participated BOOLEAN DEFAULT FALSE,description TEXT);";
+            createT = "CREATE TABLE IF NOT EXISTS events (id SERIAL PRIMARY KEY,event_info_id SERIAL NOT NULL,CONSTRAINT fk_event_info_id FOREIGN KEY(event_info_id) REFERENCES event_infos(id),user_id SERIAL NOT NULL,CONSTRAINT fk_events_user_id FOREIGN KEY(user_id) REFERENCES users(id),color VARCHAR(7) NOT NULL,reminder_interval INT DEFAULT 0,  reminder_time_frame INT );";
+            smt = connection.createStatement();
+            smt.executeUpdate(createT);
+
+            createT = "CREATE TABLE IF NOT EXISTS reminders (id SERIAL PRIMARY KEY,event_id SERIAL,CONSTRAINT fk_reminders_event_id FOREIGN KEY(event_id) REFERENCES events(id),remind_at TIMESTAMP WITH TIME ZONE NOT NULL,showed_reminder BOOLEAN DEFAULT FALSE);";
+            smt = connection.createStatement();
+            smt.executeUpdate(createT);
+
+
+            createT = "CREATE TABLE IF NOT EXISTS rsvps (id SERIAL PRIMARY KEY,event_id SERIAL,CONSTRAINT fk_rsvps_event_id FOREIGN KEY(event_id) REFERENCES events(id),rsvp_sender_id SERIAL,CONSTRAINT fk_rsvps_sender_id FOREIGN KEY(rsvp_sender_id) REFERENCES users(id),rsvp_receiver_id SERIAL,CONSTRAINT fk_rsvps_receiver_id FOREIGN KEY(rsvp_receiver_id) REFERENCES users(id),  received_at TIMESTAMP WITH TIME ZONE NOT NULL,is_participated BOOLEAN DEFAULT FALSE,description TEXT);";
             smt = connection.createStatement();
             smt.executeUpdate(createT);
 
